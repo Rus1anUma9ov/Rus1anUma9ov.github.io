e7f0de3d71c4ad7e3c3187d0ba0c79ae5ca8cbc8
Rus1anUma9ov <98204048+Rus1anUma9ov@users.noreply.github.com>
2022-01-24 22:14:46 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('load',function(){
+    const elBtn = this.document.querySelector('header .header__shop .header__shop-card .btn');
+    elBtn.addEventListener('click',function(){
+        this.style.boxShadow = 'none';
+    })
+})
+
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 8,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+
+        responsive:{
+            320:{
+                items:2,
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 1000,
+            },
+            576:{
+                items:4,
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 1000,
+            },
+            768:{
+                items:5,
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 1000,
+            },
+            992:{
+                items:8,
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 1000,
+            }
+        }
+    });
+  });
